e8fe408b9e2242203752931820f4d5e156c6e83d
Alexander Kutsakov <askutsakov@edu.hse.ru>
2021-12-23 19:53:59 +0300

    new linter version

--- bot.py
+++ bot.py
@@ -6,14 +6,17 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     """Sends a greeting to user"""
     bot.reply_to(message, "Howdy, how are you doing?")
 
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     """Answers the message"""
     bot.reply_to(message, message.text)
+
 
 bot.infinity_polling()
