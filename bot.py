bb9ea968ce47309aeb18b53647351f0664aeaba0
Alexander Kutsakov <askutsakov@edu.hse.ru>
2021-12-23 15:53:33 +0300

    Fixed functions

--- bot.py
+++ bot.py
@@ -7,10 +7,12 @@ bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	"""Sends a greeting to user"""
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	"""Answers the message"""
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling()
